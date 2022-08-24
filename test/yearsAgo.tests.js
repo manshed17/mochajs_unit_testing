3a74ec53aed48f1528981287f6053a6b55041cbe
manshed17 <manshed0017@gmail.com>
2022-08-24 16:31:35 +0200

    edits

--- test/yearsAgo.tests.js
+++ test/yearsAgo.tests.js
@@ -3,7 +3,7 @@ describe('Test yearsAgo Function' , function(){
         assert.equal((new Date().getFullYear() - 1998), yearsAgo(1998));
     });
 
-    it('should return "30" if called with "1998"' , function(){
+    it('should return "30" if called with "1992"' , function(){
         assert.equal((new Date().getFullYear() - 1992), yearsAgo(1992))
 
     });
